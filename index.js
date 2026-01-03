0d052eac5cb82ee1aeea4a7826b586373ba367a5
codedevel25 <251635138+codedevel25@users.noreply.github.com>
2026-01-03 15:21:14 +0530

    Added deadline API

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const deadline = new Date('31 Dec, 2026')
+
 app.get('/', (req, res) => {
   res.send('Hello Vercel Express Server!');
+});
+
+app.get('/api/deadline', (req, res) => {
+  res.send({
+    secondsLeft: Math.round((deadline.getTime() - new Date(Date.now()).getTime()) / 1000),
+    milliSecondsLeft: deadline.getTime() - new Date(Date.now()).getTime(),
+    deadline
+  });
 });
 
 app.listen(PORT, () => {
