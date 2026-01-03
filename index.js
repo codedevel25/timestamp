8c446442cfe61af39246790e7bff1d5c496e62fe
codedevel25 <251635138+codedevel25@users.noreply.github.com>
2026-01-03 16:35:36 +0530

    Fixed ui deployment filder in vercel

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 const deadline = new Date('31 Dec, 2026');
 
 app.use(cors())
-app.use("/", express.static(__dirname + '/ui/dist/demo/browser'));
+app.use(express.static('public'));
 
 app.get('/api/deadline', (req, res) => {
   res.send({
